4cd27137ce6681405a5562a4349acb2f0bb34f73
trichoip <107303342+trichoip@users.noreply.github.com>
2024-01-31 17:53:10 +0700

    Update claim jwt

---
+++ src/ShipperStation.Domain/Constants/ClaimUsers.cs
@@ -0,0 +1,10 @@
+﻿namespace ShipperStation.Domain.Constants;
+public struct ClaimUsers
+{
+    public const string Id = "id";
+    public const string Email = "email";
+    public const string AvatarUrl = "avatar_url";
+    public const string FullName = "full_name";
+    public const string PhoneNumber = "phone_number";
+    public const string UserName = "user_name";
+}
