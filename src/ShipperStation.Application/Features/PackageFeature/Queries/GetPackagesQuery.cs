543e820d0edcbdbb5b7024a325e9fdf412eea19e
trichoip <107303342+trichoip@users.noreply.github.com>
2024-03-06 22:51:57 +0700

    Create and get packages

---
+++ src/ShipperStation.Application/Features/PackageFeature/Queries/GetPackagesQuery.cs
@@ -0,0 +1,39 @@
+﻿using LinqKit;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using ShipperStation.Application.Features.PackageFeature.Models;
+using ShipperStation.Application.Models.Pages;
+using ShipperStation.Domain.Entities;
+using ShipperStation.Domain.Enums;
+using ShipperStation.Shared.Pages;
+using System.Linq.Expressions;
+
+namespace ShipperStation.Application.Features.PackageFeature.Queries;
+public sealed record GetPackagesQuery : PaginationRequest<Package>, IRequest<PaginatedResponse<PackageResponse>>
+{
+    public string? Name { get; set; }
+    public PackageStatus? Status { get; set; }
+    public PackageType Type { get; set; }
+
+    [BindNever]
+    public Guid UserId { get; set; }
+
+    public override Expression<Func<Package, bool>> GetExpressions()
+    {
+        Expression = Expression.And(_ => string.IsNullOrWhiteSpace(Name) || EF.Functions.Like(_.Name, $"%{Name}%"));
+        Expression = Expression.And(_ => !Status.HasValue || _.Status == Status);
+
+        if (Type == PackageType.Sender)
+        {
+            Expression = Expression.And(_ => _.SenderId == UserId);
+        }
+
+        if (Type == PackageType.Receiver)
+        {
+            Expression = Expression.And(_ => _.ReceiverId == UserId);
+        }
+
+        return Expression;
+    }
+}
---
+++ src/ShipperStation.WebApi/Controllers/PackagesController.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShipperStation.Application.Common.Constants;
+using ShipperStation.Application.Features.PackageFeature.Commands;
+using ShipperStation.Application.Features.PackageFeature.Models;
+using ShipperStation.Application.Features.PackageFeature.Queries;
+using ShipperStation.Application.Models;
+using ShipperStation.Shared.Pages;
+
+namespace ShipperStation.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PackagesController(ISender sender) : ControllerBase
+{
+    [HttpGet]
+    [Authorize]
+    public async Task<ActionResult<PaginatedResponse<PackageResponse>>> GetPackages(
+        [FromQuery] GetPackagesQuery query,
+        CancellationToken cancellationToken)
+    {
+        return await sender.Send(query, cancellationToken);
+    }
+
+    //[HttpGet("{id}")]
+    //public async Task<ActionResult<PackageResponse>> GetPackageById(int id, CancellationToken cancellationToken)
+    //{
+    //    return await sender.Send(new GetPackageByIdQuery(id), cancellationToken);
+    //}
+
+    [HttpPost]
+    [Authorize(Roles = Policies.StationManager_Or_Staff)]
+    public async Task<ActionResult<MessageResponse>> CreatePackage(
+        CreatePackageCommand command,
+        CancellationToken cancellationToken)
+    {
+        return await sender.Send(command, cancellationToken);
+    }
+
+    //[HttpPut("{id}")]
+    //public async Task<ActionResult<MessageResponse>> UpdatePackage(int id, UpdatePackageCommand command, CancellationToken cancellationToken)
+    //{
+    //    return await sender.Send(command with { Id = id }, cancellationToken);
+    //}
+
+    //[HttpDelete("{id}")]
+    //public async Task<ActionResult<MessageResponse>> DeletePackage(int id, CancellationToken cancellationToken)
+    //{
+    //    return await sender.Send(new DeletePackageCommand(id), cancellationToken);
+    //}
+}
