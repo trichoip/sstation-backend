8d998bd38b360c4eec0b110fafc49113e19f110d
trichoip <107303342+trichoip@users.noreply.github.com>
2024-03-12 17:52:26 +0700

    check out package

---
+++ src/ShipperStation.Application/Features/PackageFeature/Commands/PaymentPackageCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+using ShipperStation.Application.Models;
+
+namespace ShipperStation.Application.Features.PackageFeature.Commands;
+public sealed record PaymentPackageCommand(Guid Id) : IRequest<MessageResponse>;
