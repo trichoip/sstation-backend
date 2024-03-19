d847d6b04061ba1dc3baf9ab07622de43de8db1e
trichoip <107303342+trichoip@users.noreply.github.com>
2024-03-19 21:09:02 +0700

    Update manager

--- src/ShipperStation.Application/Features/Managers/Commands/UpdateStoreManagerCommand.cs
+++ src/ShipperStation.Application/Features/Managers/Commands/UpdateStoreManagerCommand.cs
@@ -1,16 +1,15 @@
 ﻿using MediatR;
 using ShipperStation.Application.Models;
+using System.Text.Json.Serialization;
 
 namespace ShipperStation.Application.Features.Managers.Commands;
-public sealed record CreateStoreManagerCommand : IRequest<MessageResponse>
+public sealed record UpdateStoreManagerCommand : IRequest<MessageResponse>
 {
-    public string UserName { get; set; } = default!;
+    [JsonIgnore]
+    public Guid Id { get; set; }
     public string Password { get; set; } = default!;
     public string? FullName { get; set; }
     public string? AvatarUrl { get; set; }
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
-
-    public bool IsActive => true;
-
 }
---
+++ src/ShipperStation.Application/Features/Managers/Handlers/UpdateStoreManagerCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using ShipperStation.Application.Common.Exceptions;
+using ShipperStation.Application.Common.Resources;
+using ShipperStation.Application.Features.Managers.Commands;
+using ShipperStation.Application.Models;
+using ShipperStation.Domain.Entities.Identities;
+
+namespace ShipperStation.Application.Features.Managers.Handlers;
+internal sealed class UpdateStoreManagerCommandHandler(UserManager<User> userManager) : IRequestHandler<UpdateStoreManagerCommand, MessageResponse>
+{
+    public async Task<MessageResponse> Handle(UpdateStoreManagerCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByIdAsync(request.Id.ToString());
+
+        if (user is null)
+        {
+            throw new NotFoundException(nameof(User), request.Id);
+        }
+
+        request.Adapt(user);
+        await userManager.UpdateAsync(user);
+
+        if (!await userManager.CheckPasswordAsync(user, request.Password))
+        {
+            var result = await userManager.RemovePasswordAsync(user);
+
+            if (!result.Succeeded)
+            {
+                throw new ValidationBadRequestException(result.Errors);
+            }
+
+            result = await userManager.AddPasswordAsync(user, request.Password);
+
+            if (!result.Succeeded)
+            {
+                throw new ValidationBadRequestException(result.Errors);
+            }
+        }
+
+        return new MessageResponse(Resource.UpdatedSuccess);
+    }
+}
