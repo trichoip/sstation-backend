88df8f72ba5a0420e821588f6f8166ac3c89f9a7
trichoip <107303342+trichoip@users.noreply.github.com>
2024-04-22 06:49:22 +0700

    Update QR payment

--- src/ShipperStation.Application/Features/PackageFeature/Events/SendNotifyPackageExprireReceiveEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿using ShipperStation.Domain.Common;
-
-namespace ShipperStation.Application.Features.PackageFeature.Events;
-public sealed record SendNotifyPackageExprireReceiveEvent : BaseEvent
-{
-    public Guid PackageId { get; set; }
-}
--- src/ShipperStation.Application/Features/PackageFeature/Models/QrPaymentPackage.cs
+++ src/ShipperStation.Application/Features/PackageFeature/Models/QrPaymentPackage.cs
@@ -1,6 +1,5 @@
 ﻿using ShipperStation.Application.Extensions;
 using ShipperStation.Application.Features.Pricings.Models;
-using ShipperStation.Domain.Enums;
 using ShipperStation.Shared.Extensions;
 using System.Text.Json.Serialization;
 
@@ -9,29 +8,13 @@ public sealed record QrPaymentPackage
 {
     public Guid Id { get; set; }
     public string? Name { get; set; }
-    public string? Description { get; set; }
     public double PriceCod { get; set; }
-    public bool IsCod { get; set; }
-    public PackageStatus Status { get; set; }
-    public double Weight { get; set; }
-    public double Height { get; set; }
-    public double Width { get; set; }
-    public double Length { get; set; }
+    [JsonIgnore]
     public double Volume { get; set; }
-
-    public Guid SenderId { get; set; }
-    //public UserResponse Sender { get; set; } = default!;
-
-    public Guid ReceiverId { get; set; }
-    //public UserResponse Receiver { get; set; } = default!;
-
-    public string Location { get; set; } = default!;
-
+    [JsonIgnore]
     public double TotalHours { get; set; }
     public double TotalPrice => PriceCod + ServiceFee;
-
     public double ServiceFee => Pricing is null ? 0 : PackageExtensions.CalculateServiceFee(Volume, TotalHours, Pricing.PricePerUnit, Pricing.UnitDuration);
-
     public string FormatTotalPrice => TotalPrice.FormatMoney();
     public string FormatServiceFee => ServiceFee.FormatMoney();
 
@@ -40,8 +23,4 @@ public sealed record QrPaymentPackage
 
     [JsonIgnore]
     public ICollection<PricingResponse> Pricings { get; set; } = new HashSet<PricingResponse>();
-
-    public ICollection<PackageImageResponse> PackageImages { get; set; } = new HashSet<PackageImageResponse>();
-
-    //public ICollection<PackageStatusHistoryResponse> PackageStatusHistories { get; set; } = new HashSet<PackageStatusHistoryResponse>();
 }
