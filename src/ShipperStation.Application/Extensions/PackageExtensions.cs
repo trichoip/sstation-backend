81b44d6dd0fe52877411843a3b391de47dffb566
trichoip <107303342+trichoip@users.noreply.github.com>
2024-06-01 13:51:41 +0700

    Update service fees

--- src/ShipperStation.Application/Extensions/PackageExtensions.cs
+++ src/ShipperStation.Application/Extensions/PackageExtensions.cs
@@ -3,8 +3,8 @@ public static class PackageExtensions
 {
     public static double CalculateServiceFee(double volume, double totalDays, double price)
     {
-        var serviceFee = (volume * price * totalDays) / 1000;
-
+        //var serviceFee = (volume * price * totalDays) / 1000;
+        var serviceFee = volume + (price * totalDays);
         return serviceFee;
     }
 }
